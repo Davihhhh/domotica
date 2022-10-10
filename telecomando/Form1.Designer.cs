7db4e80104c97baddcaaaeb481500938d177fae5
Davihhhh <davidelin2005@gmail.com>
2022-10-10 10:44:47 +0200

    Aggiungere i file di progetto.

---
+++ telecomando/Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace telecomando
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nove = new System.Windows.Forms.Button();
+            this.zero = new System.Windows.Forms.Button();
+            this.tre = new System.Windows.Forms.Button();
+            this.due = new System.Windows.Forms.Button();
+            this.uno = new System.Windows.Forms.Button();
+            this.sei = new System.Windows.Forms.Button();
+            this.cinque = new System.Windows.Forms.Button();
+            this.quattro = new System.Windows.Forms.Button();
+            this.sette = new System.Windows.Forms.Button();
+            this.otto = new System.Windows.Forms.Button();
+            this.AbbassaCan = new System.Windows.Forms.Button();
+            this.AumentaCan = new System.Windows.Forms.Button();
+            this.OnOff = new System.Windows.Forms.Button();
+            this.AlzaVol = new System.Windows.Forms.Button();
+            this.AbbassaVol = new System.Windows.Forms.Button();
+            this.textBoxVolume = new System.Windows.Forms.TextBox();
+            this.textBoxCanaleAttuale = new System.Windows.Forms.TextBox();
+            this.textBoxCanale = new System.Windows.Forms.TextBox();
+            this.textBoxVolumeAttuale = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonStato = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nove
+            // 
+            this.nove.BackColor = System.Drawing.Color.Silver;
+            this.nove.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.nove.Location = new System.Drawing.Point(438, 312);
+            this.nove.Name = "nove";
+            this.nove.Size = new System.Drawing.Size(49, 36);
+            this.nove.TabIndex = 0;
+            this.nove.Text = "9";
+            this.nove.UseVisualStyleBackColor = false;
+            this.nove.Click += new System.EventHandler(this.nove_Click);
+            // 
+            // zero
+            // 
+            this.zero.BackColor = System.Drawing.Color.Silver;
+            this.zero.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.zero.Location = new System.Drawing.Point(318, 187);
+            this.zero.Name = "zero";
+            this.zero.Size = new System.Drawing.Size(49, 36);
+            this.zero.TabIndex = 1;
+            this.zero.Text = "0";
+            this.zero.UseVisualStyleBackColor = false;
+            this.zero.Click += new System.EventHandler(this.zero_Click);
+            // 
+            // tre
+            // 
+            this.tre.BackColor = System.Drawing.Color.Silver;
+            this.tre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.tre.Location = new System.Drawing.Point(438, 229);
+            this.tre.Name = "tre";
+            this.tre.Size = new System.Drawing.Size(49, 36);
+            this.tre.TabIndex = 2;
+            this.tre.Text = "3";
+            this.tre.UseVisualStyleBackColor = false;
+            this.tre.Click += new System.EventHandler(this.tre_Click);
+            // 
+            // due
+            // 
+            this.due.BackColor = System.Drawing.Color.Silver;
+            this.due.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.due.Location = new System.Drawing.Point(378, 229);
+            this.due.Name = "due";
+            this.due.Size = new System.Drawing.Size(49, 36);
+            this.due.TabIndex = 3;
+            this.due.Text = "2";
+            this.due.UseVisualStyleBackColor = false;
+            this.due.Click += new System.EventHandler(this.due_Click);
+            // 
+            // uno
+            // 
+            this.uno.BackColor = System.Drawing.Color.Silver;
+            this.uno.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.uno.Location = new System.Drawing.Point(318, 229);
+            this.uno.Name = "uno";
+            this.uno.Size = new System.Drawing.Size(49, 36);
+            this.uno.TabIndex = 4;
+            this.uno.Text = "1";
+            this.uno.UseVisualStyleBackColor = false;
+            this.uno.Click += new System.EventHandler(this.uno_Click);
+            // 
+            // sei
+            // 
+            this.sei.BackColor = System.Drawing.Color.Silver;
+            this.sei.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.sei.Location = new System.Drawing.Point(438, 271);
+            this.sei.Name = "sei";
+            this.sei.Size = new System.Drawing.Size(49, 36);
+            this.sei.TabIndex = 4;
+            this.sei.Text = "6";
+            this.sei.UseVisualStyleBackColor = false;
+            this.sei.Click += new System.EventHandler(this.sei_Click);
+            // 
+            // cinque
+            // 
+            this.cinque.BackColor = System.Drawing.Color.Silver;
+            this.cinque.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.cinque.Location = new System.Drawing.Point(378, 270);
+            this.cinque.Name = "cinque";
+            this.cinque.Size = new System.Drawing.Size(49, 36);
+            this.cinque.TabIndex = 6;
+            this.cinque.Text = "5";
+            this.cinque.UseVisualStyleBackColor = false;
+            this.cinque.Click += new System.EventHandler(this.cinque_Click);
+            // 
+            // quattro
+            // 
+            this.quattro.BackColor = System.Drawing.Color.Silver;
+            this.quattro.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.quattro.Location = new System.Drawing.Point(318, 270);
+            this.quattro.Name = "quattro";
+            this.quattro.Size = new System.Drawing.Size(49, 36);
+            this.quattro.TabIndex = 7;
+            this.quattro.Text = "4";
+            this.quattro.UseVisualStyleBackColor = false;
+            this.quattro.Click += new System.EventHandler(this.quattro_Click);
+            // 
+            // sette
+            // 
+            this.sette.BackColor = System.Drawing.Color.Silver;
+            this.sette.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.sette.Location = new System.Drawing.Point(318, 312);
+            this.sette.Name = "sette";
+            this.sette.Size = new System.Drawing.Size(49, 36);
+            this.sette.TabIndex = 8;
+            this.sette.Text = "7";
+            this.sette.UseVisualStyleBackColor = false;
+            this.sette.Click += new System.EventHandler(this.sette_Click);
+            // 
+            // otto
+            // 
+            this.otto.BackColor = System.Drawing.Color.Silver;
+            this.otto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.otto.Location = new System.Drawing.Point(378, 312);
+            this.otto.Name = "otto";
+            this.otto.Size = new System.Drawing.Size(49, 36);
+            this.otto.TabIndex = 9;
+            this.otto.Text = "8";
+            this.otto.UseVisualStyleBackColor = false;
+            this.otto.Click += new System.EventHandler(this.otto_Click);
+            // 
+            // AbbassaCan
+            // 
+            this.AbbassaCan.BackColor = System.Drawing.Color.Gainsboro;
+            this.AbbassaCan.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.AbbassaCan.Location = new System.Drawing.Point(438, 187);
+            this.AbbassaCan.Name = "AbbassaCan";
+            this.AbbassaCan.Size = new System.Drawing.Size(49, 36);
+            this.AbbassaCan.TabIndex = 10;
+            this.AbbassaCan.Text = "↓";
+            this.AbbassaCan.UseVisualStyleBackColor = false;
+            this.AbbassaCan.Click += new System.EventHandler(this.AbbassaCan_Click);
+            // 
+            // AumentaCan
+            // 
+            this.AumentaCan.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.AumentaCan.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.AumentaCan.Location = new System.Drawing.Point(438, 145);
+            this.AumentaCan.Name = "AumentaCan";
+            this.AumentaCan.Size = new System.Drawing.Size(49, 36);
+            this.AumentaCan.TabIndex = 11;
+            this.AumentaCan.Text = "↑";
+            this.AumentaCan.UseVisualStyleBackColor = false;
+            this.AumentaCan.Click += new System.EventHandler(this.AumentaCan_Click);
+            // 
+            // OnOff
+            // 
+            this.OnOff.BackColor = System.Drawing.Color.Lime;
+            this.OnOff.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.OnOff.Location = new System.Drawing.Point(318, 145);
+            this.OnOff.Name = "OnOff";
+            this.OnOff.Size = new System.Drawing.Size(49, 36);
+            this.OnOff.TabIndex = 12;
+            this.OnOff.Text = "On/Off";
+            this.OnOff.UseVisualStyleBackColor = false;
+            this.OnOff.Click += new System.EventHandler(this.OnOff_Click);
+            // 
+            // AlzaVol
+            // 
+            this.AlzaVol.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.AlzaVol.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.AlzaVol.Location = new System.Drawing.Point(378, 145);
+            this.AlzaVol.Name = "AlzaVol";
+            this.AlzaVol.Size = new System.Drawing.Size(49, 36);
+            this.AlzaVol.TabIndex = 13;
+            this.AlzaVol.Text = "+";
+            this.AlzaVol.UseVisualStyleBackColor = false;
+            this.AlzaVol.Click += new System.EventHandler(this.AlzaVol_Click);
+            // 
+            // AbbassaVol
+            // 
+            this.AbbassaVol.BackColor = System.Drawing.Color.Gainsboro;
+            this.AbbassaVol.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.AbbassaVol.Location = new System.Drawing.Point(378, 187);
+            this.AbbassaVol.Name = "AbbassaVol";
+            this.AbbassaVol.Size = new System.Drawing.Size(49, 36);
+            this.AbbassaVol.TabIndex = 14;
+            this.AbbassaVol.Text = "-";
+            this.AbbassaVol.UseVisualStyleBackColor = false;
+            this.AbbassaVol.Click += new System.EventHandler(this.AbbassaVol_Click);
+            // 
+            // textBoxVolume
+            // 
+            this.textBoxVolume.Enabled = false;
+            this.textBoxVolume.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.textBoxVolume.Location = new System.Drawing.Point(115, 117);
+            this.textBoxVolume.Name = "textBoxVolume";
+            this.textBoxVolume.Size = new System.Drawing.Size(100, 29);
+            this.textBoxVolume.TabIndex = 15;
+            this.textBoxVolume.Text = "Volume";
+            // 
+            // textBoxCanaleAttuale
+            // 
+            this.textBoxCanaleAttuale.Enabled = false;
+            this.textBoxCanaleAttuale.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.textBoxCanaleAttuale.Location = new System.Drawing.Point(115, 319);
+            this.textBoxCanaleAttuale.Name = "textBoxCanaleAttuale";
+            this.textBoxCanaleAttuale.Size = new System.Drawing.Size(100, 29);
+            this.textBoxCanaleAttuale.TabIndex = 17;
+            // 
+            // textBoxCanale
+            // 
+            this.textBoxCanale.Enabled = false;
+            this.textBoxCanale.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.textBoxCanale.Location = new System.Drawing.Point(115, 266);
+            this.textBoxCanale.Name = "textBoxCanale";
+            this.textBoxCanale.Size = new System.Drawing.Size(100, 29);
+            this.textBoxCanale.TabIndex = 18;
+            this.textBoxCanale.Text = "Canale";
+            // 
+            // textBoxVolumeAttuale
+            // 
+            this.textBoxVolumeAttuale.Enabled = false;
+            this.textBoxVolumeAttuale.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.textBoxVolumeAttuale.Location = new System.Drawing.Point(115, 161);
+            this.textBoxVolumeAttuale.Name = "textBoxVolumeAttuale";
+            this.textBoxVolumeAttuale.Size = new System.Drawing.Size(100, 29);
+            this.textBoxVolumeAttuale.TabIndex = 19;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MistyRose;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.button1.Location = new System.Drawing.Point(366, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Vai";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // buttonStato
+            // 
+            this.buttonStato.Enabled = false;
+            this.buttonStato.Location = new System.Drawing.Point(115, 40);
+            this.buttonStato.Name = "buttonStato";
+            this.buttonStato.Size = new System.Drawing.Size(100, 23);
+            this.buttonStato.TabIndex = 21;
+            this.buttonStato.UseVisualStyleBackColor = true;
+            this.buttonStato.Click += new System.EventHandler(this.buttonStato_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(685, 452);
+            this.Controls.Add(this.buttonStato);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxVolumeAttuale);
+            this.Controls.Add(this.textBoxCanale);
+            this.Controls.Add(this.textBoxCanaleAttuale);
+            this.Controls.Add(this.textBoxVolume);
+            this.Controls.Add(this.AbbassaVol);
+            this.Controls.Add(this.AlzaVol);
+            this.Controls.Add(this.OnOff);
+            this.Controls.Add(this.AumentaCan);
+            this.Controls.Add(this.AbbassaCan);
+            this.Controls.Add(this.otto);
+            this.Controls.Add(this.sette);
+            this.Controls.Add(this.quattro);
+            this.Controls.Add(this.cinque);
+            this.Controls.Add(this.sei);
+            this.Controls.Add(this.uno);
+            this.Controls.Add(this.due);
+            this.Controls.Add(this.tre);
+            this.Controls.Add(this.zero);
+            this.Controls.Add(this.nove);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button nove;
+        private System.Windows.Forms.Button zero;
+        private System.Windows.Forms.Button tre;
+        private System.Windows.Forms.Button due;
+        private System.Windows.Forms.Button uno;
+        private System.Windows.Forms.Button sei;
+        private System.Windows.Forms.Button cinque;
+        private System.Windows.Forms.Button quattro;
+        private System.Windows.Forms.Button sette;
+        private System.Windows.Forms.Button otto;
+        private System.Windows.Forms.Button AbbassaCan;
+        private System.Windows.Forms.Button AumentaCan;
+        private System.Windows.Forms.Button OnOff;
+        private System.Windows.Forms.Button AlzaVol;
+        private System.Windows.Forms.Button AbbassaVol;
+        private System.Windows.Forms.TextBox textBoxVolume;
+        private System.Windows.Forms.TextBox textBoxCanaleAttuale;
+        private System.Windows.Forms.TextBox textBoxCanale;
+        private System.Windows.Forms.TextBox textBoxVolumeAttuale;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonStato;
+    }
+}
+
---
+++ telecomando/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace telecomando
+{
+    public partial class Form1 : Form
+    {
+        private Telecomando tel;
+        int[] canale = new int[3];
+        
+        int cont = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            tel = new Telecomando();
+            textBoxVolumeAttuale.Text = tel.GetVolume().ToString();
+            textBoxCanaleAttuale.Text = tel.GetCanale().ToString();
+            setstatocolor();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void setstatocolor()
+        {
+            if (tel.GetStato())
+                buttonStato.BackColor = Color.Green;
+            else
+                buttonStato.BackColor = Color.Red;
+        }
+        private void OnOff_Click(object sender, EventArgs e)
+        {
+            tel.InvertiStatoTelevisione();
+            setstatocolor();
+        }
+
+
+        private void AlzaVol_Click(object sender, EventArgs e)
+        {
+            tel.AumentaVolume();
+        }
+        private void AbbassaVol_Click(object sender, EventArgs e)
+        {
+            tel.DiminuisciVolume();
+        }
+
+
+        private void AumentaCan_Click(object sender, EventArgs e)
+        {
+            tel.AumentaCanale();
+        }
+        private void AbbassaCan_Click(object sender, EventArgs e)
+        {
+            tel.DiminuisciCanale();
+        }
+
+        private void zero_Click(object sender, EventArgs e)
+        {
+            if (checkacceso())
+            {
+                canale[cont] = 0;
+                cont++;
+            }
+            else { }
+        }
+        private void uno_Click(object sender, EventArgs e)
+        {
+            if (checkacceso())
+            {
+                canale[cont] = 1;
+                cont++;
+            }
+            else { }
+        }
+
+        private void due_Click(object sender, EventArgs e)
+        {
+            if (checkacceso())
+            {
+                canale[cont] = 2;
+                cont++;
+            }
+            else { }
+        }
+
+        private void tre_Click(object sender, EventArgs e)
+        {
+            if (checkacceso())
+            {
+                canale[cont] = 3;
+                cont++;
+            }
+            else { }
+        }
+
+        private void quattro_Click(object sender, EventArgs e)
+        {
+            if (checkacceso())
+            {
+                canale[cont] = 4;
+                cont++;
+            }
+            else { }
+        }
+
+        private void cinque_Click(object sender, EventArgs e)
+        {
+            if (checkacceso())
+            {
+                canale[cont] = 5;
+                cont++;
+            }
+            else { }
+        }
+
+        private void sei_Click(object sender, EventArgs e)
+        {
+            if (checkacceso())
+            {
+                canale[cont] = 6;
+                cont++;
+            }
+            else { }
+        }
+
+        private void sette_Click(object sender, EventArgs e)
+        {
+            if (checkacceso())
+            {
+                canale[cont] = 7;
+                cont++;
+            }
+            else { }
+        }
+
+        private void otto_Click(object sender, EventArgs e)
+        {
+            if (checkacceso())
+            {
+                canale[cont] = 8;
+                cont++;
+            }
+            else { }
+        }
+
+        private void nove_Click(object sender, EventArgs e)
+        {
+            if (checkacceso())
+            {
+                canale[cont] = 9;
+                cont++;
+            }
+            else { }
+        }
+
+        private void TempoDigitazione_Tick(object sender, EventArgs e)
+        {
+            textBoxCanale.Text = Convert.ToString(canale);
+        }
+
+        private bool checkacceso()
+        {
+            bool check = tel.GetStato();
+            return check;
+        }
+
+        private void buttonStato_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
