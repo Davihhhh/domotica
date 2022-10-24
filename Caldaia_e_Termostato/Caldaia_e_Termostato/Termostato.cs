3e79c88906a2eb9cca668c87c0585a0b0f0b1fbd
Davihhhh <davidelin2005@gmail.com>
2022-10-24 08:15:11 +0200

    prova

---
+++ Caldaia_e_Termostato/Caldaia_e_Termostato/Termostato.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caldaia_e_Termostato
+{
+    public class Termostato
+    {
+        string Produttore;
+        string Modello;
+        string Data_Installazione;
+        int Numero_Serie;
+    }
+}
